7f79d91112964f0761575c5c85c329582600bf1e
armankhan0313 <arman.khan@remcare.co.uk>
2024-10-18 01:32:43 +0530

    fix

--- resources/views/banners/fields.blade.php
+++ resources/views/banners/fields.blade.php
@@ -95,7 +95,7 @@
 
 <script>
     $(document).ready(function() {
-        let sliderCount = {{ count($banner->bannerImages) }}; // Start with the count of existing banner images
+        let sliderCount = {{ ($banner)?count($banner->bannerImages):0 }}; // Start with the count of existing banner images
 
         function createEditorForDescription(rowId) {
             ClassicEditor
